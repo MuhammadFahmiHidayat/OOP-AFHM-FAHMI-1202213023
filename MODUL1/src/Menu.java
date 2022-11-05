5ba171b0ce01b7d32a66b6561308d36b80bb9b45
MuhammadFahmiHidayat <m.fahmihidayatt@gmail.com>
2022-11-05 07:56:22 +0700

    Modul1

---
+++ MODUL1/src/Menu.java
@@ -0,0 +1,17 @@
+public class Menu {
+    String name;
+    String category;
+    String price;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCategory(String category){
+        this.category = category;
+    }
+    public void setPrice(Integer price){
+        this.price = price;
+    }
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+}
---
+++ MODUL1/src/Server.java
@@ -0,0 +1,25 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Selamat Datang di Restoran EAD")
+        System.out.println("Silahkan Register Terlebih Dahulu")
+        System.out.println("==================================")
+        User us = new User();
+
+
+        // TODO Create Database
+
+        // TODO Create Menu
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
